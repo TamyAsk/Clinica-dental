e6561fe0a622e174ac4f43c9c5aa9af93082f40a
TamyAsk <ramsesochoa11@gmail.com>
2024-10-16 11:11:55 -0700

    primera fase

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -40,13 +40,18 @@
         </div>
 
         <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
             <x-primary-button class="ms-4">
                 {{ __('Register') }}
             </x-primary-button>
         </div>
+
+        <div class="mt-4 text-center">
+            <p style="color: white">¿No tienes una cuenta? 
+                <a href="{{ route('login') }}" class="underline text-sm text-white-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    Inicia Sesion
+                </a>
+            </p>
+        </div>
+
     </form>
 </x-guest-layout>
